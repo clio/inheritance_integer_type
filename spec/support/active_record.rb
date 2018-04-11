a634b777b4dbbf26208c89d8578509277794fee1
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2018-04-11 18:54:33 +0000

    Repair specs

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -2,10 +2,10 @@ require 'active_record'
 Dir["#{File.dirname(__FILE__)}/migrations/*.rb"].each {|f| require f}
 
 config = {
-  :adapter => "mysql",
+  :adapter => "mysql2",
   :host => "localhost",
   :database => "inheritance_integer_type_test",
-  :username => "iit",
-  :password => ""
+  :username => "root",
+  :password => "iasEcerdyetvanyet"
 }
 ActiveRecord::Base.establish_connection(config)
